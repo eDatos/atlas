4a704c744c3a7c3d2e24b5f9ae03b58ca636b955
amonfue <amonfue@arte-consultores.com>
2020-03-17 12:57:54 +0000

    EDATOS-3138 - Añadir traducción en español
    > Se añaden las traducciones en español.
    
    > Se borran ficheros innecesarios.

--- lib/Views/OtherButton.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import MenuButton from "terriajs/lib/ReactViews/Map/MenuButton";
-
-export default function OtherButton() {
-  return <MenuButton caption="Other" href="other.html" />;
-}
