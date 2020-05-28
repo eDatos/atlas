dd85c50b4e0b7b10266cbe49bcaa3ea0842f2cd4
Miguel Jonás Bravo García <mbragar@arte-consultores.com>
2020-05-28 08:13:33 +0100

    EDATOS-3146 - Instalación del entorno de estadísticas en la nueva máquina OVH
    > Se sustituye en los scripts de despliegue la IP de la nueva máquina por el nombre de la misma

--- etc/deploy/scripts/demo/deploy.sh
+++ etc/deploy/scripts/demo/deploy.sh
@@ -5,9 +5,9 @@ TRANSFER_PATH=$HOME_PATH/tmp
 ENV_CONF=$HOME_PATH/env
 DEPLOY_TARGET_PATH=/servers/estadisticas/terria
 
-scp -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" -r etc/deploy/config/demo/* deploy@192.168.10.16:$ENV_CONF
-scp -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" -r target/terria-*.jar deploy@192.168.10.16:$TRANSFER_PATH/terria.jar
-ssh -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" deploy@192.168.10.16 <<EOF
+scp -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" -r etc/deploy/config/demo/* deploy@estadisticas.arte.internal:$ENV_CONF
+scp -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" -r target/terria-*.jar deploy@estadisticas.arte.internal:$TRANSFER_PATH/terria.jar
+ssh -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" deploy@estadisticas.arte.internal <<EOF
 
     ###
     # TERRIA
