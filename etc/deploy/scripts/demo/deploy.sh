f17d7098563b807842a799e61417c30d847a2450
Francisco Javier Rodríguez García <frodgar@arte-consultores.com>
2020-10-16 15:58:18 +0100

    NONE - Modificación comando scp/ssh para evitar Host key verification failed.

--- etc/deploy/scripts/demo/deploy.sh
+++ etc/deploy/scripts/demo/deploy.sh
@@ -5,9 +5,9 @@ TRANSFER_PATH=$HOME_PATH/tmp
 ENV_CONF=$HOME_PATH/env
 DEPLOY_TARGET_PATH=/servers/estadisticas/terria
 
-scp -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" -r etc/deploy/config/demo/* deploy@estadisticas.arte.internal:$ENV_CONF
-scp -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" -r target/terria-*.jar deploy@estadisticas.arte.internal:$TRANSFER_PATH/terria.jar
-ssh -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" deploy@estadisticas.arte.internal <<EOF
+scp -o StrictHostKeyChecking=no -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" -r etc/deploy/config/demo/* deploy@estadisticas.arte.internal:$ENV_CONF
+scp -o StrictHostKeyChecking=no -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" -r target/terria-*.jar deploy@estadisticas.arte.internal:$TRANSFER_PATH/terria.jar
+ssh -o StrictHostKeyChecking=no -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" deploy@estadisticas.arte.internal <<EOF
 
     ###
     # TERRIA
